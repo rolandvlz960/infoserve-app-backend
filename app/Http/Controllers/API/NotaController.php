df91104e7199c3603f0da0f6a5f98d3b7c89a6bc
Mauricio Ruiz Diaz Maciel <mauri.rd@gmail.com>
2018-11-07 09:56:26 -0300

    aautocut test

--- app/Http/Controllers/API/NotaController.php
+++ app/Http/Controllers/API/NotaController.php
@@ -214,7 +214,7 @@ class NotaController extends Controller
             $printer->text("                       \n");
             $printer->text("                       \n");
             $printer->feed(1);
-            $printer->text("i");
+            $printer->text("m");
             $printer->close();
         }
     }
