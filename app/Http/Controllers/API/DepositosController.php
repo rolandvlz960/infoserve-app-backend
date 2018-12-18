97c4763a2b9da6359d4d0c1b0159b470923e63be
Mauricio Ruiz Diaz Maciel <mauri.rd@gmail.com>
2018-12-18 15:50:03 -0300

    Order by deposito

--- app/Http/Controllers/API/DepositosController.php
+++ app/Http/Controllers/API/DepositosController.php
@@ -14,6 +14,7 @@ class DepositosController extends Controller
             'deposito',
             'nome'
         )->disponible()
+            ->orderBy('deposito', 'asc')
             ->get();
     }
 }
