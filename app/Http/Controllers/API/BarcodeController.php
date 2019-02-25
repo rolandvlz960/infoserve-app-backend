581b3ca51d5cb931078935e4a0b01378574ffe61
Mauricio Ruiz Diaz Maciel <mauri.rd@gmail.com>
2019-02-25 11:05:26 -0300

    Add transaction al enviar nota

--- app/Http/Controllers/API/BarcodeController.php
+++ app/Http/Controllers/API/BarcodeController.php
@@ -28,25 +28,32 @@ class BarcodeController extends Controller
 
     public function save(Request $request)
     {
-        $ultimaNota = Colecta::orderBy('NOTA', 'desc')->first();
-        $numNota = 1;
-        if (!is_null($ultimaNota)) {
-            $numNota = $ultimaNota->NOTA + 1;
-        }
-        if ($request->has('items')) {
-            foreach ($request->items as $item) {
-                Colecta::create([
-                    'NOTA' => $numNota + 1,
-                    'PRODUTO' => $item['producto'],
-                    'USUARIO' => $request->usuario,
-                    'DEPOSITO' => $request->deposito,
-                    'OPERACAO' => $request->operacion,
-                    'QUANTIDADE' => $item['cantidad'],
-                    'DATA' => DB::raw('NOW()'),
-                    'HORA' => DB::raw("DATE_FORMAT(NOW(), '%H:%i:%s')"),
-                ]);
+        $transactionOk = false;
+        DB::transaction(function() use($request, &$transactionOk) {
+            $ultimaNota = Colecta::orderBy('NOTA', 'desc')->first();
+            $numNota = 1;
+            if (!is_null($ultimaNota)) {
+                $numNota = $ultimaNota->NOTA + 1;
             }
+            if ($request->has('items')) {
+                foreach ($request->items as $item) {
+                    Colecta::create([
+                        'NOTA' => $numNota + 1,
+                        'PRODUTO' => $item['producto'],
+                        'USUARIO' => $request->usuario,
+                        'DEPOSITO' => $request->deposito,
+                        'OPERACAO' => $request->operacion,
+                        'QUANTIDADE' => $item['cantidad'],
+                        'DATA' => DB::raw('NOW()'),
+                        'HORA' => DB::raw("DATE_FORMAT(NOW(), '%H:%i:%s')"),
+                    ]);
+                }
+            }
+            $transactionOk = true;
+        });
+        if ($transactionOk) {
+            return response(200);
         }
-        return response(200);
+        return response(500);
     }
 }
